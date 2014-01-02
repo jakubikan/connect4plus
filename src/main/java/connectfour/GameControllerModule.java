8f044f609bf1d49a752bfeef4c0402d80e8ecb9b
jakub Werner <jack@jaxxnet.de>
2014-01-02 20:59:18 +0100

    added tui and gui for injection

--- src/main/java/connectfour/GameControllerModule.java
+++ src/main/java/connectfour/GameControllerModule.java
@@ -1,12 +1,18 @@
 package connectfour;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 import connectfour.controller.GameController;
 import connectfour.controller.IController;
 import connectfour.persistence.ISaveGameDAO;
+import connectfour.persistence.couchdb.SaveGameCouchDbDAO;
 import connectfour.persistence.db4o.SaveGameDb4oDAO;
 import connectfour.persistence.hibernate.SaveGameDbHibernate;
 import connectfour.persistence.hibernate.SaveGameHibernate;
+import connectfour.ui.gui.swing.SwingGUI;
+import connectfour.ui.tui.TUI;
+import connectfour.util.observer.IObserver;
 import connectfour.util.observer.IObserverWithArguments;
 
 public class GameControllerModule extends AbstractModule {
@@ -16,6 +22,9 @@ public class GameControllerModule extends AbstractModule {
         bind(IController.class).to(GameController.class);
         bind(IObserverWithArguments.class).to(GameController.class);
         //bind(ISaveGameDAO.class).to(SaveGameDbHibernate.class);
-        bind(ISaveGameDAO.class).to(SaveGameDb4oDAO.class);
+        bind(ISaveGameDAO.class).to(SaveGameCouchDbDAO.class);
+        //bind(ISaveGameDAO.class).to(SaveGameDb4oDAO.class);
+        bind(IObserver.class).annotatedWith(Names.named("tui")).to(TUI.class);
+        bind(IObserver.class).annotatedWith(Names.named("gui")).to(SwingGUI.class);
     }
 }
