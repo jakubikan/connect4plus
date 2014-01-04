90a3c275a6509cc93df9310784577a1f53f602c5
jakub Werner <jack@jaxxnet.de>
2014-01-04 15:08:01 +0100

    stateless db4o

--- src/main/java/connectfour/persistence/db4o/SaveGameDb4oDAO.java
+++ src/main/java/connectfour/persistence/db4o/SaveGameDb4oDAO.java
@@ -1,6 +1,7 @@
 package connectfour.persistence.db4o;
 
 import com.db4o.Db4oEmbedded;
+import com.db4o.EmbeddedObjectContainer;
 import com.db4o.ObjectContainer;
 import com.db4o.ext.DatabaseFileLockedException;
 import com.db4o.query.Predicate;
@@ -15,20 +16,20 @@ import java.util.logging.Logger;
 
 @Singleton
 public class SaveGameDb4oDAO implements ISaveGameDAO {
-	private ObjectContainer db;
     private static final Logger log = Logger.getLogger(SaveGameDb4oDAO.class.getName());
+
+	private ObjectContainer db;
 
     /**
      * Opens the database by default.
      */
 	public SaveGameDb4oDAO() {
-            openDB();
     }
 
     @Override
     public final void openDB() {
         try {
-            db = Db4oEmbedded.openFile("savegame.data");
+            db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),"savegame.data");
         } catch (DatabaseFileLockedException e) {
             log.info(e.getMessage());
             throw new DatabaseFileLockedException("Database Locked");
@@ -43,18 +44,46 @@ public class SaveGameDb4oDAO implements ISaveGameDAO {
 		saveGame.getPlayer1().setGameField(null);
 		saveGame.getPlayer2().setGameField(null);
 
-		deleteSaveGameIfExists(saveGame.getSaveGameName());
 
+		if (deleteSaveGameIfExists(saveGame.getSaveGameName())) {
+            log.info("Game correctly deleted");
+        } else {
+            log.info("Savegame did not exist.");
+        }
+
+
+        openDB();
 		db.store(saveGame);
+        closeDB();
 	}
 
 	@Override
 	public boolean deleteSaveGameIfExists(final String saveGameName) {
-        SaveGame sg = loadSaveGame(saveGameName);
-        if (sg != null) {
-			db.delete(sg);
+        openDB();
+        List<SaveGame> listISaveGame = db.query(new Predicate<SaveGame>() {
+            private static final long serialVersionUID = 1L;
+
+            public boolean match(SaveGame sg) {
+                return sg.getSaveGameName().equals(saveGameName);
+            }
+        });
+
+        SaveGame saveGame = null;
+        if (listISaveGame.size() > 0) {
+            saveGame = listISaveGame.get(0);
+        }
+        if (saveGame != null) {
+            try {
+                db.delete(saveGame);
+                db.commit();
+            } catch (Exception e) {
+                log.info(e.getMessage());
+            } finally {
+                closeDB();
+            }
 			return true;
 		}
+        closeDB();
 		return false;
 	}
 	
@@ -65,6 +94,7 @@ public class SaveGameDb4oDAO implements ISaveGameDAO {
 
 	@Override
 	public SaveGame loadSaveGame(final String saveGameName) {
+        openDB();
 		List<SaveGame> listISaveGame = db.query(new Predicate<SaveGame>() {
 			private static final long serialVersionUID = 1L;
 
@@ -73,14 +103,18 @@ public class SaveGameDb4oDAO implements ISaveGameDAO {
 			}
 		});
 
+        SaveGame saveGame = null;
 		if (listISaveGame.size() > 0) {
-			return listISaveGame.get(0);
+			saveGame = listISaveGame.get(0);
 		}
-		return null;
+        closeDB();
+
+		return saveGame;
 	}
 
 	@Override
 	public List<String> getAllSaveGames() {
+        openDB();
 		Iterator<SaveGame> it = db.query(SaveGame.class).iterator();
 		List<String> allSaveGames = new LinkedList<String>();
 		
@@ -88,12 +122,18 @@ public class SaveGameDb4oDAO implements ISaveGameDAO {
 			SaveGame sg = it.next();
 			allSaveGames.add(sg.getSaveGameName());
 		}
-		
+
+        closeDB();
 		return allSaveGames;
 	}
 	
 	@Override
 	public void closeDB() {
+        try {
 		db.close();
+        } catch (Exception e) {
+            log.info("Something went went while closing");
+
+        }
 	}
 }
